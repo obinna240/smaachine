07d31970890bb7013eb5b9bd288ca1f6b2098ab3
obinna240 <obinna240@yahoo.co.uk>
2023-01-21 14:12:14 +0000

    remove tests

--- src/main/java/com/statemachine/smaachine/machines/model/ModeratedMessage.java
+++
@@ -1,9 +0,0 @@
-//package com.statemachine.smaachine.machines.model;
-//
-//public class ModeratedMessage {
-//    private boolean isModerated;
-//    private String id;
-//    private Message message;
-//    @Enumerated(EnumType.STRING)
-//    private ModerationState moderationEvent;
-//}
