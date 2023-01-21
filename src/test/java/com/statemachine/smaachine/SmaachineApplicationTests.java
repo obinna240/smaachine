a19245d0a28132c4cdb54a1c82ee42910f412645
obinna240 <obinna240@yahoo.co.uk>
2023-01-21 14:03:35 +0000

    cleanup test

--- src/test/java/com/statemachine/smaachine/SmaachineApplicationTests.java
+++ src/test/java/com/statemachine/smaachine/SmaachineApplicationTests.java
@@ -1,9 +1,9 @@
 package com.statemachine.smaachine;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class SmaachineApplicationTests {
 
 	@Test
