775ef9570306de3f9e8452eeb29efef9fc62fc47
obinna240 <obinna240@yahoo.co.uk>
2023-01-21 14:02:02 +0000

    making code easy

--- src/main/java/com/statemachine/smaachine/SmaachineApplication.java
+++ src/main/java/com/statemachine/smaachine/SmaachineApplication.java
@@ -1,13 +1,11 @@
 package com.statemachine.smaachine;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class SmaachineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmaachineApplication.class, args);
+
+		System.out.println("This is a test");
 	}
 
 }
